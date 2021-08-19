7ed4e59b5cec50c1a7f80a6b8e719d290794b27d
Ken Qiu <kenin.qiu@gmail.com>
2021-08-19 10:08:31 -0400

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,17 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MenuComponent } from './menu/menu.component';
 import { AdvComponent } from './adv/adv.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ChangeComponent } from './change/change.component';
+import { SalaryDataComponent } from './salary-data/salary-data.component';
+import { CustomersExperienceComponent } from './customers-experience/customers-experience.component';
+import { BestInClassComponent } from './best-in-class/best-in-class.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, MenuComponent, AdvComponent],
+  declarations: [HomeComponent, MenuComponent, AdvComponent, CarouselComponent, ChangeComponent, SalaryDataComponent, CustomersExperienceComponent, BestInClassComponent, FooterComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
---
+++ src/app/home/salary-data/salary-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-data',
+  templateUrl: './salary-data.component.html',
+  styleUrls: ['./salary-data.component.scss']
+})
+export class SalaryDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
