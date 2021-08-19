548bdfac360380f482cf490f688b7d99906ed0e8
Ken Qiu <kenin.qiu@gmail.com>
2021-08-19 06:57:37 -0400

    fix

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { MenuComponent } from './menu/menu.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, MenuComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
