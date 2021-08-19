1aa615208a4262726aabc7c7155bf199b4233f7d
Ken Qiu <kenin.qiu@gmail.com>
2021-08-19 19:00:31 -0400

    fi

---
+++ src/app/research/browse-by-states/browse-by-states.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-by-states',
+  templateUrl: './browse-by-states.component.html',
+  styleUrls: ['./browse-by-states.component.scss']
+})
+export class BrowseByStatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/research/research.module.ts
+++ src/app/research/research.module.ts
@@ -13,11 +13,17 @@ import { SchoolComponent } from './school/school.component';
 import { SearchSalaryComponent } from './search-salary/search-salary.component';
 import { BrowseAllCategoriesComponent } from './browse-all-categories/browse-all-categories.component';
 import { FindYourMarketWorthComponent } from './find-your-market-worth/find-your-market-worth.component';
+import { BrowsePopularJobsComponent } from './browse-popular-jobs/browse-popular-jobs.component';
+import { LearnAboutEmployersComponent } from './learn-about-employers/learn-about-employers.component';
+import { ResearchSchoolsAndDegreesComponent } from './research-schools-and-degrees/research-schools-and-degrees.component';
+import { GetCareerTipsAndAdviceComponent } from './get-career-tips-and-advice/get-career-tips-and-advice.component';
+import { BrowseByStatesComponent } from './browse-by-states/browse-by-states.component';
+import { WhatAmIWorthComponent } from './what-am-i-worth/what-am-i-worth.component';
 
 
 
 @NgModule({
-  declarations: [ResearchComponent, EmployerComponent, JobComponent, DegreeComponent, CertificationComponent, SkillComponent, IndustryComponent, SchoolComponent, SearchSalaryComponent, BrowseAllCategoriesComponent, FindYourMarketWorthComponent],
+  declarations: [ResearchComponent, EmployerComponent, JobComponent, DegreeComponent, CertificationComponent, SkillComponent, IndustryComponent, SchoolComponent, SearchSalaryComponent, BrowseAllCategoriesComponent, FindYourMarketWorthComponent, BrowsePopularJobsComponent, LearnAboutEmployersComponent, ResearchSchoolsAndDegreesComponent, GetCareerTipsAndAdviceComponent, BrowseByStatesComponent, WhatAmIWorthComponent],
   imports: [
     ResearchRoutingModule,
     LayoutModule,
