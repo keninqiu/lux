2bdacf341ff967a3e9a032e14769a650949b5d67
Ken Qiu <kenin.qiu@gmail.com>
2021-08-19 15:44:29 -0400

    fix

--- src/app/home/salary-data/salary-data.component.ts
+++ src/app/home/salary-data/salary-data.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./salary-data.component.scss']
 })
 export class SalaryDataComponent implements OnInit {
-
+  slideIndex: number;
+  interval: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.slideIndex = 0;
+    this.interval = setInterval(() => {
+      this.slideIndex = (this.slideIndex + 1) % 3;
+    },4500);
+  }
+
+  changeSlideIndex(index: number) {
+    this.slideIndex = index;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
 }
