40e9594a544f966ac424ccc91d0360a952560bd9
Ken Qiu <kenin.qiu@gmail.com>
2021-08-19 07:04:32 -0400

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MenuComponent } from './menu/menu.component';
+import { AdvComponent } from './adv/adv.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, MenuComponent],
+  declarations: [HomeComponent, MenuComponent, AdvComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
