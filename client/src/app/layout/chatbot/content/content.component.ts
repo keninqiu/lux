ea7c0150429c6a5ccd422a105349051c86652499
Ken Qiu <kenin.qiu@gmail.com>
2021-09-02 19:35:59 -0400

    fg

--- client/src/app/layout/chatbot/content/content.component.ts
+++ client/src/app/layout/chatbot/content/content.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [
     './content.component.scss',
     '../../../../assets/css/home/7.004395ae.chunk.css',
-    '../../../../assets/css/home/19.4751f621.chunk.css'
+    '../../../../assets/css/home/19.4751f621.chunk.css',
+    '../../../../assets/css/home/28.a35d8593.chunk.css',
+    '../../../../assets/css/home/0.d96f2542.chunk.css'
   ]
 })
 export class ChatbotContentComponent implements OnInit {
