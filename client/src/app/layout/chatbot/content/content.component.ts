b7ead8fb5f5c46af8d6ce2a4d6b2ead59a9291e2
Ken Qiu <kenin.qiu@gmail.com>
2021-09-01 23:19:59 -0400

    f

---
+++ client/src/app/layout/chatbot/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-content',
+  templateUrl: './content.component.html',
+  styleUrls: [
+    './content.component.scss',
+    '../../../../assets/css/home/7.004395ae.chunk.css',
+    '../../../../assets/css/home/19.4751f621.chunk.css'
+  ]
+})
+export class ChatbotContentComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
-
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { ChatbotContentComponent } from './chatbot/content/content.component';
 @NgModule({
-  declarations: [FooterComponent, MenuComponent],
+  declarations: [
+    FooterComponent, 
+    MenuComponent,
+    ChatbotComponent,
+    ChatbotContentComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [FooterComponent, MenuComponent]
+  exports: [
+    FooterComponent, 
+    MenuComponent,
+    ChatbotComponent
+  ]
 })
 export class LayoutModule { }
